4013ffba39a94ce78d322f5e9f4b0ac789c87ffd
Unknown <i@do.not.know>
2004-09-09 15:18:41 +0000

    This commit was manufactured by cvs2svn to create tag 'debian_version_1_31_0-7'.

--- libs/iterator/doc/generate.py
+++ libs/iterator/doc/generate.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python
-# Copyright David Abrahams 2004. Use, modification and distribution is
-# subject to the Boost Software License, Version 1.0. (See accompanying
-# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #
 # Generate html, TeX, and PDF versions of all the source files
@@ -16,7 +13,7 @@ if 0:
     for s in sources:
         syscmd('boosthtml %s' % s)
 else:        
-    extensions = ('html', 'pdf')
+    extensions = ('html', 'tex', 'pdf')
 
     if len(sys.argv) > 1:
         extensions = sys.argv[1:]
@@ -26,7 +23,7 @@ else:
           for s in sources 
         ]
 
-    print 'make %s' % ' '.join(all)
-    syscmd('make %s' % ' '.join(all))
+    print 'make -k %s' % ' '.join(all)
+    syscmd('make -k %s' % ' '.join(all))
 
 
